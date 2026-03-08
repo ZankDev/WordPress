236ca9f628d16fedce9927d619243afce0bf83da
Jordan <78556733+ZankDev@users.noreply.github.com>
2026-03-08 08:32:35 -0700

    Fix cookie domain and set WP_HOME and WP_SITEURL constants
    Add COOKIE_DOMAIN, WP_HOME and WP_SITEURL definitions to allow login via non-HTTPS and fix cookie error. Also includes DB creds.

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,8 @@
 <?php
 // Generated wp-config.php for Dokploy deployment
 
-define('DB_NAME', WWWorldpress_db');
-define('DB_USER', 'yardenpasso1978@gmail.com');
+define('DB_NAME', ''Worldpress_db'
+define('DB_USER', 'yard'Worldpress_db'.com');
 define('DB_PASSWORD', 'yarden06');
 define('DB_HOST', 'servi-wolrdpress-site-jordan-glhp8t');
 define('DB_CHARSET', 'utf8');
@@ -15,10 +15,8 @@ define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
 define('WP_HOME', 'http://lp.servi.co.il');
 define('WP_SITEURL', 'http://lp.servi.co.il');
 
-// Absolute path to the WordPress directory
 if (!defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/');
 }
 
-// Sets up WordPress vars and included files
 require_once ABSPATH . 'wp-settings.php';
