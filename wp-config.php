9fa22e144182cf1237c2dacaf59ba104a36af49d
Jordan <78556733+ZankDev@users.noreply.github.com>
2026-03-08 08:25:00 -0700

    Fix DB_NAME and DB_HOST; add COOKIE_DOMAIN, WP_HOME, and WP_SITEURL constants
    Corrected database name and added site URL definitions.

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,7 @@
 <?php
 // Generated wp-config.php for Dokploy deployment
 
-define('DB_NAME', 'Worldpress_db');
+define('DB_NAME', WWWorldpress_db');
 define('DB_USER', 'yardenpasso1978@gmail.com');
 define('DB_PASSWORD', 'yarden06');
 define('DB_HOST', 'servi-wolrdpress-site-jordan-glhp8t');
@@ -11,6 +11,9 @@ define('DB_COLLATE', '');
 $table_prefix = 'wp_';
 
 define('WP_DEBUG', false);
+define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
+define('WP_HOME', 'http://lp.servi.co.il');
+define('WP_SITEURL', 'http://lp.servi.co.il');
 
 // Absolute path to the WordPress directory
 if (!defined('ABSPATH')) {
